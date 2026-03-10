d857d4c171c94b325bc22de68b1b264f0b05073e
Naoyu Kinami <nkinami00@mylangara.ca>
2026-03-09 17:06:36 -0700

    test

--- math.test.js
+++ math.test.js
@@ -4,6 +4,10 @@ test('2 + 3 = 5', () => {
     expect(addition(2,3)).toBe(5)
 })
 
+test('23 + 32 = 55', () => {
+    expect(addition(23,32)).toBe(55)
+})
+
 test.skip('10 + 90 = 100', () => {
     expect(addition(10, 90)).toBe(100)
 })
